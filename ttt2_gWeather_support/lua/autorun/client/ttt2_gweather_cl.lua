2e59dc336de1fdd34f7aba3d588df5bd2a41283d
milkwxter <ethan.r.bates2003@gmail.com>
2025-03-08 17:20:56 -0600

    Removed HUD on startup, too much clutter

--- ttt2_gWeather_support/lua/autorun/client/ttt2_gweather_cl.lua
+++ ttt2_gWeather_support/lua/autorun/client/ttt2_gweather_cl.lua
@@ -2,3 +2,7 @@ net.Receive( "ttt2_tell_about_weather", function( len, ply )
 	local forecast_name = net.ReadString()
 	chat.AddText(Color( 175, 175, 255 ), "[TODAY'S FORECAST]: ", Color(255, 255, 255), forecast_name)
 end )
+
+hook.Add("InitPostEntity", "TTT2gWeatherDisableHUD", function()
+	RunConsoleCommand("gw_enablehud", "0")
+end)
